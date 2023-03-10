91ad4221dfe57c505faa9f2480ab1bbd3cc2961b
Antônio Apolinário Gonzaga Neto <a.apolinario.neto@gmail.com>
2023-03-09 22:12:07 -0300

    barrra de pesquisa
    barra de pesquisa em javascript, funcionando. ao colocar determinada letra aparece uma sugestão de acordo com oq foi escrito.

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
-const  itens = [
-    "Bulbasaur"
-    ,"Ivysaur"
-    ,"Venusaur"
-    ,"Charmander "
-    ,"Charmeleon"
-    ,"Charizard"
-    ,"Squirtle"
-    ,"Wartortle"
-    ,"Blastoise"
-    ,"Caterpie"
-    ,"Metapod"]
+let input = document.getElementById("pesquisa").value
+input = input.toLowerCase()
+let x = document.getElementsByClassName("lista")
+function pesquisa_poke() {
 
-    const searchInput = document.getElementById("pesquisa")
-    const searchResults = document.getElementById("search-results")
-
-    searchInput.addEventListener("keyup", (e) => {
-        const searchTerm = e.target.value.toLowerCase()
-        const results = []
-
-        itens.forEach((item) => {
-            const itemLowerCase = item.toLowerCase ()
-            if (itemLowerCase.includes(searchTerm)) {
-                results.push(item)
-            }
-        })
-        showResults(results)
-    })
-
-    function showResults(results) {
-        searchResults.innerHtml = ""
-
-        results.forEach((result) => {
-            const li = document.createElement("li")
-            li.textContent = result
-            searchResults.appendChild(li)
-        })
+    for (let i = 0; i <x.length; i++) {
+        if(!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none"
+            console.log("1")
+        } else {
+            x[i].style.display = "list-item"
+            console.log("2")
+        }
     }
+}
+function click() {
+    x.style.display = "list-item"
+}
